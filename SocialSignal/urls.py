ccebec00b5e046e7de4f94142fac160727a882f5
Mansiv75 <mv072004@gmail.com>
2024-11-30 15:27:09 +0530

    setup authentication

--- SocialSignal/urls.py
+++ SocialSignal/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_page, name="login"),
+    path('register/', register_page, name="register"),
+    
 ]
